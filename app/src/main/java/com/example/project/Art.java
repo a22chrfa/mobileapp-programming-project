569da589df83ba700b93ffd0939c54dddfd39f6f
ChristianFast <112157470+ChristianFast@users.noreply.github.com>
2023-05-17 11:31:15 +0200

    Added Java class "Art" and added comment containing actual data (to reference the structure of the data later).

---
+++ app/src/main/java/com/example/project/Art.java
@@ -0,0 +1,68 @@
+package com.example.project;
+
+/*
+[{
+	"ID": "22chr-001",
+	"name": "Potatisätarna",
+	"type": "a22chrfa",
+	"company": "",
+	"location": "Amsterdam",
+	"category": "Vincent van Gogh",
+	"size": 0,
+	"cost": 0,
+	"auxdata": "https://sv.wikipedia.org/wiki/Potatis%C3%A4tarna"
+}, {
+	"ID": "22chr-002",
+	"name": "Madonnan i grottan",
+	"type": "a22chrfa",
+	"company": "",
+	"location": "Paris",
+	"category": "Leonardo da Vinci",
+	"size": 0,
+	"cost": 0,
+	"auxdata": "https://sv.wikipedia.org/wiki/Madonnan_i_grottan"
+}, {
+	"ID": "22chr-003",
+	"name": "Nattvarden",
+	"type": "a22chrfa",
+	"company": "",
+	"location": "Milano",
+	"category": "Leonardo da Vinci",
+	"size": 0,
+	"cost": 0,
+	"auxdata": "https://sv.wikipedia.org/wiki/Nattvarden"
+}, {
+	"ID": "22chr-004",
+	"name": "Slaget vid Anghiari",
+	"type": "a22chrfa",
+	"company": "",
+	"location": "Paris",
+	"category": "Peter Paul Rubens",
+	"size": 0,
+	"cost": 0,
+	"auxdata": "https://sv.wikipedia.org/wiki/Slaget_vid_Anghiari"
+}, {
+	"ID": "22chr-005",
+	"name": "Skriet",
+	"type": "a22chrfa",
+	"company": "",
+	"location": "Oslo",
+	"category": "Edvard Munch",
+	"size": 0,
+	"cost": 0,
+	"auxdata": "https://sv.wikipedia.org/wiki/Skriet"
+}, {
+	"ID": "22chr-006",
+	"name": "Mona Lisa",
+	"type": "a22chrfa",
+	"company": "",
+	"location": "Paris",
+	"category": "Leonardo da Vinci",
+	"size": 0,
+	"cost": 0,
+	"auxdata": "https://sv.wikipedia.org/wiki/Mona_Lisa"
+}]
+*/
+
+public class Art {
+}
